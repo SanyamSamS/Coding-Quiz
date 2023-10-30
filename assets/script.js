a0b5ee6368ecf422e9cc3d2c4b3b06055ee5f059
SanyamSamS <sanyam.edu@gmail.com>
2023-10-30 17:53:44 -0400

    added functions for ending the quiz and calculating the total user score

--- assets/script.js
+++ assets/script.js
@@ -39,13 +39,13 @@ var questions = [
     }
 ];
 
-// Event listeners 
-startButton.addEventListener("click", startQuiz)
 
 // Start quiz function 
+startButton.addEventListener("click", startQuiz)
+
 function startQuiz() {
-    startScreen.style.display = "none";
-    quizScreen.style.display = "block";
+    startScreen.style.display = "none"; // Hide start screen
+    quizScreen.style.display = "block"; // Display quiz screen
     showQuestion();
     startTimer();
 }
@@ -65,30 +65,72 @@ function startTimer() {
 
 // Show questions function 
 function showQuestion() {
-    if (currentQuestion < questions.length){
+    if (currentQuestion < questions.length) {
         var currentQuestionEl = questions[currentQuestion];
 
         // Update question text
         var questionEl = document.querySelector("#quiz-screen h3");
         questionEl.textContent = currentQuestionEl.question;
 
+        // Update options text
         var optionButtons = document.querySelectorAll("#quiz-screen .option-button");
-        optionButtons.forEach(function(button, index) {
-            if (currentQuestionEl.choices[index]) {
-                button.textContent = currentQuestionEl.choices[index];
-            }
-        });
+
+        // Event listener for each option button
+        optionButtons.forEach(function (button, index) {
+            button.addEventListener('click', function() {
+                var selectedAnswer = index;
+                userAnswers[currentQuestion] = selectedAnswer;
+
+                currentQuestion++; // Next question
+                
+                if (currentQuestion < questions.length) {
+                    showQuestion();
+                } else {
+                    endQuiz();
+                }
+        })
+    });
     } else {
         endQuiz();
     }
 }
 
+// Store user answers 
+var userAnswers = [];
+
+
+// Calculate score function 
+function calculateScore() {
+    var score = 0;
+    for (var i = 0; i < questions.length; i++) {
+        if (userAnswers[i] === questions[i].answer) {
+            score += 10;
+        }
+    }
+    return score;
+}
+
+
 
 // End quiz function 
+function endQuiz() {
+    clearInterval(timer); // Ends timer
+
+    quizScreen.style.display = "none"; // Hides quiz screen 
+    endScreen.style.display = "block"; // Display end screen
+
+    var totalScore = calculateScore();
+
+    // Update score text 
+    document.getElementById("final-score").textContent = totalScore;
+}
 
 
 // Show high scores functions
-
+function showHighScores() {
+    var highScores = JSON.parse(localStorage.getItem("highScores"));
+    var highScoresList = document.getElementById("highscores-list");
+}
 
 // Go back to quiz function 
 
